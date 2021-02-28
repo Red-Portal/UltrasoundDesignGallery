f4b0d2c3b5511394d935b3cb00cc0b53fdfc8d03
Ray Kim <msca8h@naver.com>
2021-02-28 23:07:42 +0900

    add blaze interface header

---
+++ src/misc/blaze.hpp
@@ -0,0 +1,33 @@
+
+/*
+ * Copyright (C) 2021  Ray Kim
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __US_GALLERY_BLAZE_HPP__
+#define __US_GALLERY_BLAZE_HPP__
+
+#define BLAZE_BLAS_MODE 1
+#define BLAZE_BLAS_IS_64BIT 1
+#define BLAZE_BLAS_IS_PARALLEL 1
+#define BLAZE_USE_SHARED_MEMORY_PARALLELIZATION 0
+#define BLAZE_USE_VECTORIZATION 1
+#define BLAZE_DEFAULT_STORAGE_ORDER blaze::rowMajor
+
+//#define BLAZE_USE_DEBUG_MODE 1
+
+#include <blaze/Blaze.h>
+
+#endif
